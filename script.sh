6d8533a1217058be3582506be0f82a96e2eeebb4
chloe2607 <123561028+chloe2607@users.noreply.github.com>
2023-11-29 15:08:13 +0100

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,17 @@
+sudo apt update
+sudo dpkg --configure -a
+sudo apt install apt-transport-https -y ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+docker --version
+echo "Docker installed successfully"
+# sudo apt install git -y
+# echo "Succesfully installed git"
+sudo apt install docker-compose -y
+echo "Docker compose installed successfully"
+git clone https://github.com/uzyexe/javascript-tetris.git
+cd javascript-tetris
+docker-compose up
